bff3fe792e9c6b1bfecdfd339d45713fea01624f
init.defaultBranch <benlimpic@gmail.com>
2022-02-16 17:30:15 -0800

    added: basic HTML structure, images folder, links ( ben-youtube, benlimpic.band, google -> bulldog, boston), css background gradient.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
-
 document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "More content added!";
+document.querySelector("body").appendChild(h3);
+
+const p1 = document.createElement("p");
+p1.textContent = "Stuff to say Stuff to say";
+document.querySelector("body").appendChild(p1);
+
+const p2 = document.createElement("p");
+p2.textContent = "Stuff to say Stuff to say";
+document.querySelector("body").appendChild(p2);
+
+
+
+
