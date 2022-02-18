9ea1acc282583cdf1370e88839d65882ba74a3e8
init.defaultBranch <benlimpic@gmail.com>
2022-02-17 23:21:09 -0800

    added photos and content. Structural edits.

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 
-const h2 = document.createElement("h2");
-h2.textContent = "These are some Musical Dogs!";
-document.querySelector("body").appendChild(h2);
-
-const h3 = document.createElement("h3");
-h3.textContent = "Bulldogs and Bostons Love Music";
-document.querySelector("body").appendChild(h3);
-
-const p1 = document.createElement("p");
-p1.textContent = "At least I think they do...";
-document.querySelector("body").appendChild(p1);
-
